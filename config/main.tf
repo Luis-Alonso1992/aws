207854d30b215006fe41a315dd750acda29b5565
luis.alonso@alonsops.com <133282260+luis10333@users.noreply.github.com>
2026-07-21 14:14:41 -0400

    Added Terraformf state to s3

--- config/main.tf
+++ config/main.tf
@@ -12,6 +12,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-090163643451-us-east-1-an  "
+    key            = "aws/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    
+    # Enable S3 Native State Locking (Terraform 1.10+)
+    use_lock_file  = true 
+  }
+}
+
 #Ubuntu VM
 resource "aws_instance" "web" {
     ami = "ami-0b6d9d3d33ba97d99"
